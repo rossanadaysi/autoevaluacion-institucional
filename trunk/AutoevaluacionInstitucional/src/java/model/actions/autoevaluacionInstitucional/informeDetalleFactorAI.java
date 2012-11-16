2fea847749e434102d13aba9458271ca18f3043e
oscar7685@gmail.com <oscar7685@gmail.com@61fced18-4558-098f-2216-fc57c8446f30>
2012-11-16 21:34:21 +0000

    informe x matrices corregido

--- trunk/AutoevaluacionInstitucional/src/java/model/actions/autoevaluacionInstitucional/informeDetalleFactorAI.java
+++ trunk/AutoevaluacionInstitucional/src/java/model/actions/autoevaluacionInstitucional/informeDetalleFactorAI.java
@@ -17,57 +17,47 @@ public class informeDetalleFactorAI implements Action {
         String idF = (String) request.getParameter("idF");
         sqlController conSql = new sqlController();
         Result detalleFactor = null;
-        String sql2 = "SELECT factor.id AS fid,  factor.nombre AS fno, ponderacionfactor.ponderacion AS fpo, caracteristica.id as cara, \n" +
-"                   caracteristica.nombre, ponderacioncaracteristica.nivelimportancia, ponderacioncaracteristica.ponderacion as ponderacionCara, \n" +
-"                   format(case when ( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n" +
-"                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end))) is null then  avg (   numericadocumental.evaluacion ) \n" +
-"                   else (( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n" +
-"                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end)))+avg (numericadocumental.evaluacion))/2 end ,1) as cumplimiento,\n" +
-"                    format( ponderacioncaracteristica.ponderacion * case when ( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n" +
-"                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end))) is null then  avg (   numericadocumental.evaluacion ) \n" +
-"                   else (( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n" +
-"                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end)))+avg (numericadocumental.evaluacion))/2 end , 1)as evaluacion,\n" +
-"                   format(5 * ponderacioncaracteristica.ponderacion ,1) as ideal,\n" +
-"                   format((ponderacioncaracteristica.ponderacion * case when ( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n" +
-"                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end))) is null then  avg (   numericadocumental.evaluacion ) \n" +
-"                   else (( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n" +
-"                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n" +
-"                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end)))+avg (numericadocumental.evaluacion))/2 end / ( ponderacioncaracteristica.ponderacion))*20 ,1)as relacion\n" +
-"                   FROM factor\n" +
-"                   INNER JOIN caracteristica ON caracteristica.factor_id = factor.id\n" +
-"                   INNER JOIN ponderacionfactor ON ponderacionfactor.factor_id = factor.id\n" +
-"                   INNER JOIN ponderacioncaracteristica ON ponderacioncaracteristica.caracteristica_id = caracteristica.id\n" +
-"                   INNER JOIN indicador ON indicador.caracteristica_id = caracteristica.id\n" +
-"                   LEFT JOIN numericadocumental ON numericadocumental.indicador_id = indicador.id\n" +
-"                   LEFT JOIN pregunta ON pregunta.indicador_id = indicador.id\n" +
-"                   LEFT JOIN resultadoevaluacion ON resultadoevaluacion.pregunta_id = pregunta.id\n" +
-"                   LEFT JOIN encabezado ON encabezado.id = resultadoevaluacion.encabezado_id\n" +
-"                   WHERE factor.id="+ idF+" "+ 
-"                   GROUP BY caracteristica.id;\n" +
-"";
+        String sql2 = "select c1.fid, c1.fno, c1.fpo, c1.cara, c1.nombre, c1.nivel, c1.ponderacionCara, avg(c1.cumplimiento), avg(cumplimiento)*c1.ponderacionCara, 5*c1.ponderacionCara, avg(cumplimiento)*20    from(\n"
+                + "\n"
+                + "SELECT factor.id AS fid,  factor.nombre AS fno, ponderacionfactor.ponderacion AS fpo, caracteristica.id as cara, \n"
+                + "                   caracteristica.nombre as nombre, ponderacioncaracteristica.nivelimportancia as nivel, ponderacioncaracteristica.ponderacion as ponderacionCara, \n"
+                + "                   format(case \n"
+                + "           when ( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n"
+                + "                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end))) is null \n"
+                + "           then  avg (numericadocumental.evaluacion ) \n"
+                + "           when avg (numericadocumental.evaluacion )is null \n"
+                + "           then (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n"
+                + "                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end))\n"
+                + "           else (( (sum( case when respuesta='1'  THEN 1 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='2'  THEN 2 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='3'  THEN 3 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='4'  THEN 4 ELSE 0 end)+\n"
+                + "                   sum( case when respuesta='5'  THEN 5 ELSE 0 end))/\n"
+                + "                   (count(case when (respuesta ='1' or respuesta='2' or respuesta='3' or respuesta='4' or respuesta='5') THEN 1 else null end)))+avg (numericadocumental.evaluacion))/2 \n"
+                + "       end ,2) as cumplimiento\n"
+                + "                   FROM factor\n"
+                + "                   INNER JOIN caracteristica ON caracteristica.factor_id = factor.id\n"
+                + "                   INNER JOIN ponderacionfactor ON ponderacionfactor.factor_id = factor.id\n"
+                + "                   INNER JOIN ponderacioncaracteristica ON ponderacioncaracteristica.caracteristica_id = caracteristica.id\n"
+                + "                   INNER JOIN indicador ON indicador.caracteristica_id = caracteristica.id\n"
+                + "                   LEFT JOIN numericadocumental ON numericadocumental.indicador_id = indicador.id\n"
+                + "                   LEFT JOIN pregunta ON pregunta.indicador_id = indicador.id\n"
+                + "                   LEFT JOIN resultadoevaluacion ON resultadoevaluacion.pregunta_id = pregunta.id\n"
+                + "                   WHERE factor.id="+idF+" \n"
+                + "                   group by indicador.id\n"
+                + "\n"
+                + ") as c1\n"
+                + "\n"
+                + "group by c1.cara\n"
+                + "";
         detalleFactor = conSql.CargarSql2(sql2, bd);
         session.setAttribute("detalleFactor", detalleFactor);
 
